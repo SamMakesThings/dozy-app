ed8113fecd15d5d07084e54420f3b1c785019955
Sam Stowers <phantom4d@gmail.com>
2020-02-15 15:20:08 -0600

    Remove TS compiler, add eslint & config

--- App.js
+++ App.js
@@ -13,6 +13,9 @@ import '@firebase/firestore';
 import { NavigationContainer } from '@react-navigation/native';
 GLOBAL = require('./global');
 
+// Prepare hooks for authentication functions
+const AuthContext = React.createContext();
+
 // Root app component
 export default class App extends React.Component {
   state = {
@@ -24,12 +27,93 @@ export default class App extends React.Component {
   componentDidMount() {
     this.subscribeAuthChange(fbUser => { this.setState({ user: fbUser }); console.log(this.state.user);});
     this.subscribeAuthChange(fbUser => { GLOBAL.userData = fbUser });
-  }
+  };
 
   // Update auth state as necessary (may remove this with new dynamic nav structure)
   subscribeAuthChange(callback = (user) => void 0) {
     FbAuth.onAuthStateChanged(callback);
-  }
+  };
+
+  // Copying auth functions from react-navigation guide
+  const [state, dispatch] = React.useReducer(
+    (prevState, action) => {
+      switch (action.type) {
+        case 'RESTORE_TOKEN':
+          return {
+            ...prevState,
+            userToken: action.token,
+            isLoading: false,
+          };
+        case 'SIGN_IN':
+          return {
+            ...prevState,
+            isSignout: false,
+            userToken: action.token,
+          };
+        case 'SIGN_OUT':
+          return {
+            ...prevState,
+            isSignout: true,
+            userToken: undefined,
+          };
+      }
+    },
+    {
+      isLoading: true,
+      isSignout: false,
+      userToken: null,
+    }
+  );
+
+  React.useEffect(() => {
+    // Fetch the token from storage then navigate to our appropriate place
+    const bootstrapAsync = async () => {
+      let userToken;
+
+      try {
+        userToken = await AsyncStorage.getItem('userToken');
+      } catch (e) {
+        // Restoring token failed
+      }
+
+      // After restoring token, we may need to validate it in production apps
+
+      // This will switch to the App screen or Auth screen and this loading
+      // screen will be unmounted and thrown away.
+      dispatch({ type: 'RESTORE_TOKEN', token: userToken });
+    };
+
+    bootstrapAsync();
+  }, []);
+
+  const authContext = React.useMemo(
+    () => ({
+      signIn: async data => {
+        // In a production app, we need to send some data (usually username, password) to server and get a token
+        // We will also need to handle errors if sign in failed
+        // After getting token, we need to persist the token using `AsyncStorage`
+        // In the example, we'll use a dummy token
+
+        dispatch({ type: 'SIGN_IN', token: 'dummy-auth-token' });
+      },
+      signOut: () => dispatch({ type: 'SIGN_OUT' }),
+      signUp: async data => {
+        // In a production app, we need to send user data to server and get a token
+        // We will also need to handle errors if sign up failed
+        // After getting token, we need to persist the token using `AsyncStorage`
+        // In the example, we'll use a dummy token
+
+        dispatch({ type: 'SIGN_IN', token: 'dummy-auth-token' });
+      },
+    }),
+    []
+  );
+
+  return (
+    <AuthContext.Provider value={authContext}>
+      {/* We'll render navigator content here */}
+    </AuthContext.Provider>
+  );
 
   // Render a loading screen if loading, otherwise load the main app
   render() {
