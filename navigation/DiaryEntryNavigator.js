3dc95243c1e5c2259f78acf9c3ddf9a1d65ec4a5
Sam Stowers <phantom4d@gmail.com>
2020-05-14 15:45:09 -0500

    Set entry nav file to never require updates

--- navigation/DiaryEntryNavigator.js
+++ navigation/DiaryEntryNavigator.js
@@ -5,8 +5,7 @@ import * as Screens from '../screens/DiaryEntryScreens';
 // Init and define the authentication flow for diary entry
 const DiaryEntryStack = createStackNavigator();
 
-// And create the nav components & screens. Will probably remove this later once
-// ...I have a better wizard template in place.
+// Create a stack screen for each component defined in DiaryEntryScreens
 export default function DiaryEntryNavigator() {
   return (
     <DiaryEntryStack.Navigator
@@ -16,62 +15,18 @@ export default function DiaryEntryNavigator() {
         animationEnabled: false
       }}
     >
-      <DiaryEntryStack.Screen
-        name="BedTimeInput"
-        component={Screens.BedTimeInput}
-        options={{
-          title: 'Bedtime input'
-        }}
-      />
-      <DiaryEntryStack.Screen
-        name="MinsToFallAsleepInput"
-        component={Screens.MinsToFallAsleepInput}
-        options={{
-          title: 'Minutes to fall asleep input'
-        }}
-      />
-      <DiaryEntryStack.Screen
-        name="WakeCountInput"
-        component={Screens.WakeCountInput}
-        options={{
-          title: 'Wake count input'
-        }}
-      />
-      <DiaryEntryStack.Screen
-        name="NightMinsAwakeInput"
-        component={Screens.NightMinsAwakeInput}
-        options={{
-          title: 'Night mins awake input'
-        }}
-      />
-      <DiaryEntryStack.Screen
-        name="WakeTimeInput"
-        component={Screens.WakeTimeInput}
-        options={{
-          title: 'Wake time input'
-        }}
-      />
-      <DiaryEntryStack.Screen
-        name="UpTimeInput"
-        component={Screens.UpTimeInput}
-        options={{
-          title: 'Up time input'
-        }}
-      />
-      <DiaryEntryStack.Screen
-        name="SleepRatingInput"
-        component={Screens.SleepRatingInput}
-        options={{
-          title: 'Sleep rating input'
-        }}
-      />
-      <DiaryEntryStack.Screen
-        name="TagsNotesInput"
-        component={Screens.TagsNotesInput}
-        options={{
-          title: 'Tags and notes input'
-        }}
-      />
+      {Object.keys(Screens).map((screen) => {
+        return (
+          <DiaryEntryStack.Screen
+            key={screen}
+            name={screen}
+            component={Screens[screen]}
+            options={{
+              title: screen
+            }}
+          />
+        );
+      })}
     </DiaryEntryStack.Navigator>
   );
 }
