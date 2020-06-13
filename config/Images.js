5ff68f1b6c677aa3b69207689feb052dd50832f8
Sam Stowers <phantom4d@gmail.com>
2020-06-13 11:20:36 -0500

    Remove some unused image files & imports

--- config/Images.js
+++ config/Images.js
@@ -1,9 +1,5 @@
 export default {
-  Model016: require('../assets/images/model-016.png'),
   UndrawBed: require('../assets/images/UndrawBed.png'),
-  WaveHello: require('../assets/images/WaveHello.png'),
-  Slumberwordmark: require('../assets/images/Slumberwordmark.png'),
-  DreamBg: require('../assets/images/DreamBg.png'),
   DreamBgExtended: require('../assets/images/DreamBgExtended.png'),
   WomanInBed: require('../assets/images/WomanInBed.jpeg')
 };
