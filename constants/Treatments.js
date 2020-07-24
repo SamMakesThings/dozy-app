51cd0a7f1beb0dc28f6e052ea9f109c4544fb93e
Sam Stowers <phantom4d@gmail.com>
2020-07-23 19:41:43 -0500

    Add optional/mandatory flag to treatment modules

--- constants/Treatments.js
+++ constants/Treatments.js
@@ -9,7 +9,8 @@ export default {
     subTitle: 'Gathering data for custom treatment',
     image: Images.WomanInBed,
     todos: ['Record 7 nights of sleep in your sleep diary'],
-    url: 'https://dozy.customerly.help/collecting-a-baseline-of-sleep-data/'
+    url: 'https://dozy.customerly.help/collecting-a-baseline-of-sleep-data/',
+    optional: false
   },
   SCTSRT: {
     title: 'Stimulus Control Therapy and SRT',
@@ -20,62 +21,71 @@ export default {
       'Do nothing in bed besides sleeping (no phone!)',
       'If awake for 15+ mins, get up and return later'
     ],
-    url: 'https://dozy.customerly.help/collecting-a-baseline-of-sleep-data/'
+    url: 'https://dozy.customerly.help/collecting-a-baseline-of-sleep-data/',
+    optional: false
   },
   PMR: {
     title: 'Relaxation Training & PMR',
     subTitle: 'Relax the muscles to relax the mind',
     image: Images.WomanInBed,
     todos: ['Practice PMR during the day', 'Use PMR before trying to sleep'],
-    url: 'https://dozy.customerly.help'
+    url: 'https://dozy.customerly.help',
+    optional: false
   },
   PIT: {
     title: 'Paradoxical Intention Therapy (PIT)',
     subTitle: 'To sleep, stop trying to sleep',
     image: Images.WomanInBed,
     todos: ['Practice not trying to fall asleep when in bed'],
-    url: 'https://dozy.customerly.help'
+    url: 'https://dozy.customerly.help',
+    optional: false
   },
   COG1: {
     title: 'Harmful Beliefs About Sleep',
     subTitle: 'Finding and fixing harmful perspectives',
     image: Images.WomanInBed,
     todos: ['Notice harmful thoughts and replace with healthy ones'],
-    url: 'https://dozy.customerly.help'
+    url: 'https://dozy.customerly.help',
+    optional: false
   },
   COG2: {
     title: 'Revisiting Harmful Beliefs About Sleep',
     subTitle: "Checking the progress you've made",
     image: Images.WomanInBed,
     todos: ['Notice harmful thoughts and replace with healthy ones'],
-    url: 'https://dozy.customerly.help'
+    url: 'https://dozy.customerly.help',
+    optional: false
   },
   HYG: {
     title: 'Fixing Sleep Hygiene',
     subTitle: 'Improving sleep duration & quality',
     image: Images.WomanInBed,
     todos: ['Improve sleep hygiene'],
-    url: 'https://dozy.customerly.help'
+    url: 'https://dozy.customerly.help',
+    optional: false
   },
   END: {
     title: 'Maintaining Sleep Improvements',
     subTitle: 'Strategies to prevent insomnia recurring',
     image: Images.WomanInBed,
     todos: ['Nothing to do!'],
-    url: 'https://dozy.customerly.help'
+    url: 'https://dozy.customerly.help',
+    optional: false
   },
   FIX: {
     title: 'Sticking To The Schedule',
     subTitle: 'Getting you on the path to improved sleep',
     image: Images.WomanInBed,
     todos: ['Stick to the target sleep schedule'],
-    url: 'https://dozy.customerly.help'
+    url: 'https://dozy.customerly.help',
+    optional: true
   },
   JET: {
     title: 'Fighting Jet Lag',
     subTitle: 'For travelers',
     image: Images.WomanInBed,
     todos: ['Take melatonin before trip to prepare'],
-    url: 'https://dozy.customerly.help'
+    url: 'https://dozy.customerly.help',
+    optional: true
   }
 };
