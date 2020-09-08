c80be5589f0f204a20ac10c2d99dab82ee1636ad
Sam Stowers <phantom4d@gmail.com>
2020-09-08 13:31:39 -0500

    Write remainder of submitCheckinData function

--- utilities/submitCheckinData.ts
+++ utilities/submitCheckinData.ts
@@ -7,34 +7,63 @@ interface Args {
   nextCheckinDatetime: Date;
   lastCheckinDatetime: Date;
   checkinPostponed: boolean;
-  newCheckinModule: string;
+  nextCheckinModule: string;
+  lastCheckinModule: string;
   targetTimeInBed: number;
   targetBedTime: Date;
   targetWakeTime: Date;
-  treatmentPlan: Array<object>;
 }
-
-// Does using the ref.update function overwrite maps?
-// If so, then add previous treatmentsHistory as an input and feed firestore
-// ...the updated version
 
 // Function that updates currentTreatments, nextCheckin, and treatmentsHistory in Firebase
 // Mandatory inputs: dates re: next checkin, SRT changes, treatment to be added to
 // ...currentTreatments, and data to be added to treatmentsHistory.
-async function submitCheckinData({ userId }: Args) {
-  // Initialize relevant Firebase values
-  const db: firebase.firestore.Firestore = FbLib.firestore();
-  const userDocRef = db.collection('users').doc(userId);
-  const userTreatmentsColRef = userDocRef.collection('treatmentsHistory');
+async function submitCheckinData({
+  userId,
+  checkinPostponed,
+  nextCheckinDatetime,
+  lastCheckinDatetime,
+  nextCheckinModule,
+  lastCheckinModule,
+  targetBedTime,
+  targetWakeTime,
+  targetTimeInBed
+}: Args) {
+  try {
+    // Initialize relevant Firebase values
+    const db: firebase.firestore.Firestore = FbLib.firestore();
+    const userDocRef = db.collection('users').doc(userId);
+    const userTreatmentsColRef = userDocRef.collection('treatmentsHistory');
 
-  // Update nextCheckin
-  // If postponed, update checkin date while not updating anything else
-  userDocRef.update({});
+    // Update nextCheckin & currentTreatments
+    // If postponed, update checkin date while not updating anything else
+    userDocRef.update({
+      nextCheckin: {
+        nextCheckinDatetime: nextCheckinDatetime,
+        treatmentModule: nextCheckinModule
+      },
+      'currentTreatments.nextCheckinDatetime': nextCheckinDatetime,
+      'currentTreatments.lastCheckinDatetime': lastCheckinDatetime
+    });
+    if (checkinPostponed) {
+      return false;
+    }
+    userDocRef.update({
+      'currentTreatments.currentModule': lastCheckinModule,
+      'currentTreatments.nextTreatmentModule': nextCheckinModule,
+      ['currentTreatments.' + lastCheckinModule]: lastCheckinDatetime
+    });
 
-  // Update currentTreatments
+    // Update treatmentsHistory collection by adding doc for module
+    userTreatmentsColRef.doc(lastCheckinModule).set({
+      dateStarted: lastCheckinDatetime,
+      targetBedTime: targetBedTime,
+      targetWakeTime: targetWakeTime,
+      targetTimeInBed: targetTimeInBed
+    });
+  } catch (error) {
+    console.log(error);
+  }
 
-  // Update treatmentsHistory
-
-  // Return whether or not the call was successful
+  // End function
   return true;
 }
