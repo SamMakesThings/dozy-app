38f99579f78a9293f8cd237bc46e5d360cdb105b
Sam Stowers <phantom4d@gmail.com>
2020-10-23 16:37:15 -0500

    Add 'ready' flags to tell when modules have been developed

--- constants/Treatments.js
+++ constants/Treatments.js
@@ -10,7 +10,8 @@ export default {
     image: Images.WomanInBed,
     todos: ['Record 7 nights of sleep in your sleep diary'],
     url: 'https://dozy.customerly.help/collecting-a-baseline-of-sleep-data/',
-    optional: false
+    optional: false,
+    ready: true
   },
   SCTSRT: {
     title: 'Stimulus Control Therapy and SRT',
@@ -22,7 +23,8 @@ export default {
       'If awake for 15+ mins, get up and return later'
     ],
     url: 'https://dozy.customerly.help/collecting-a-baseline-of-sleep-data/',
-    optional: false
+    optional: false,
+    ready: true
   },
   RLX: {
     title: 'Relaxation Training & PMR',
@@ -33,7 +35,8 @@ export default {
       'Use PMR before trying to sleep'
     ],
     url: 'https://dozy.customerly.help',
-    optional: false
+    optional: false,
+    ready: true
   },
   PIT: {
     title: 'Paradoxical Intention Therapy (PIT)',
@@ -41,7 +44,8 @@ export default {
     image: Images.WomanInBed,
     todos: ['Practice not trying to fall asleep when in bed'],
     url: 'https://dozy.customerly.help',
-    optional: false
+    optional: false,
+    ready: false
   },
   COG1: {
     title: 'Harmful Beliefs About Sleep',
@@ -49,7 +53,8 @@ export default {
     image: Images.WomanInBed,
     todos: ['Notice harmful thoughts and replace with healthy ones'],
     url: 'https://dozy.customerly.help',
-    optional: false
+    optional: false,
+    ready: false
   },
   COG2: {
     title: 'Revisiting Harmful Beliefs About Sleep',
@@ -57,7 +62,8 @@ export default {
     image: Images.WomanInBed,
     todos: ['Notice harmful thoughts and replace with healthy ones'],
     url: 'https://dozy.customerly.help',
-    optional: false
+    optional: false,
+    ready: false
   },
   HYG: {
     title: 'Fixing Sleep Hygiene',
@@ -65,7 +71,8 @@ export default {
     image: Images.WomanInBed,
     todos: ['Improve sleep hygiene'],
     url: 'https://dozy.customerly.help',
-    optional: false
+    optional: false,
+    ready: false
   },
   END: {
     title: 'Maintaining Sleep Improvements',
@@ -73,7 +80,8 @@ export default {
     image: Images.WomanInBed,
     todos: ['Nothing to do!'],
     url: 'https://dozy.customerly.help',
-    optional: false
+    optional: false,
+    ready: false
   },
   FIX: {
     title: 'Sticking To The Schedule',
@@ -81,7 +89,8 @@ export default {
     image: Images.WomanInBed,
     todos: ['Stick to the target sleep schedule'],
     url: 'https://dozy.customerly.help',
-    optional: true
+    optional: true,
+    ready: false
   },
   JET: {
     title: 'Fighting Jet Lag',
@@ -89,6 +98,7 @@ export default {
     image: Images.WomanInBed,
     todos: ['Take melatonin before trip to prepare'],
     url: 'https://dozy.customerly.help',
-    optional: true
+    optional: true,
+    ready: false
   }
 };
