ba3d246edd0003d7aee22e0388af3ce3a96f333a
Sam Stowers <phantom4d@gmail.com>
2020-10-31 17:25:46 -0500

    Convert TreatmentReviewScreen to TSX

--- screens/TreatmentReviewScreen.tsx
+++ screens/TreatmentReviewScreen.tsx
@@ -3,13 +3,23 @@ import React from 'react';
 import { WebView } from 'react-native-webview';
 import treatments from '../constants/Treatments';
 
+interface Props {
+  route: {
+    params: {
+      module: string;
+    };
+  };
+}
+
+const treatmentsIndexed: { [index: string]: { url: string } } = treatments;
+
 // Display a webview of the relevant knowledgebase collection or page
-export const TreatmentReviewScreen = ({ route }) => {
+export const TreatmentReviewScreen: React.FC<Props> = ({ route }) => {
   return (
     <WebView
       source={{
         // Get the module from nav params & URL from treatment constants
-        uri: treatments[route.params.module].url
+        uri: treatmentsIndexed[route.params.module].url
       }}
     />
   );
