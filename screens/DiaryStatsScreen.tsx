4d57aa0723c3430f311eb503f1d794f37162680a
Sam Stowers <phantom4d@gmail.com>
2021-04-15 16:32:10 -0500

    Get a basic sleep efficiency chart going in diary

--- screens/DiaryStatsScreen.tsx
+++ screens/DiaryStatsScreen.tsx
@@ -3,30 +3,134 @@ import { Platform, View, Text, StyleSheet } from 'react-native';
 import { DatePicker, ScreenContainer, Container } from '@draftbit/ui';
 import { scale } from 'react-native-size-matters';
 import moment from 'moment';
+import {
+  VictoryChart,
+  VictoryTheme,
+  VictoryLine,
+  VictoryAxis,
+  VictoryScatter
+} from 'victory-native';
 import { CardContainer } from '../components/CardContainer';
-import NumInputScreen from '../components/screens/NumInputScreen';
-import TextInputScreen from '../components/screens/TextInputScreen';
-import MultiButtonScreen from '../components/screens/MultiButtonScreen';
-import TagSelectScreen from '../components/screens/TagSelectScreen';
-import DateTimePickerScreen from '../components/screens/DateTimePickerScreen';
-import submitSleepDiaryData from '../utilities/submitSleepDiaryData';
 import { dozy_theme } from '../config/Themes';
 import { AuthContext } from '../utilities/authContext';
 import { Navigation, SleepLog } from '../types/custom';
 
+// Define default chart styles
+const chartStyles = {
+  chart: {
+    width: scale(320),
+    height: scale(220),
+    domainPadding: { x: 3, y: 35 }
+  },
+  axis: {
+    tickLabels: {
+      angle: -45,
+      fontSize: scale(11),
+      fill: dozy_theme.colors.light
+    },
+    grid: {
+      stroke: dozy_theme.colors.medium
+    }
+  },
+  line: {
+    data: {
+      stroke: dozy_theme.colors.primary,
+      strokeWidth: scale(4),
+      strokeLinejoin: 'round'
+    }
+  },
+  scatter: {
+    data: {
+      fill: dozy_theme.colors.primary
+    }
+  }
+};
+
 export const DiaryStatsScreen = () => {
+  const { state } = React.useContext(AuthContext);
+
+  // Trim sleepLogs to only show most recent 10
+  let recentSleepLogs = state.sleepLogs.slice(0, 10);
+  console.log(recentSleepLogs);
+
   return (
     <ScreenContainer
       style={{ backgroundColor: '#232B3F' }}
       hasSafeArea={true}
       scrollable={true}
     >
-      <Container elevation={0} useThemeGutterPadding={true}>
+      <Container
+        elevation={0}
+        useThemeGutterPadding={true}
+        style={styles.Container_Screen}
+      >
         <CardContainer style={styles.CardContainer}>
-          <Text>Streak</Text>
+          <Text
+            style={{
+              ...dozy_theme.typography.headline5,
+              ...styles.Text_CardTitle
+            }}
+          >
+            Sleep log streak: 20
+          </Text>
         </CardContainer>
-        <CardContainer style={styles.CardContainer}>
-          <Text>Sleep duration (hours)</Text>
+        <CardContainer
+          style={{ ...styles.CardContainer, paddingBottom: scale(5) }}
+        >
+          <Text
+            style={{
+              ...dozy_theme.typography.headline5,
+              ...styles.Text_CardTitle
+            }}
+          >
+            Sleep duration (hours)
+          </Text>
+          <View
+            style={{
+              alignItems: 'center',
+              marginTop: scale(-25),
+              marginLeft: scale(17)
+            }}
+          >
+            <VictoryChart
+              width={chartStyles.chart.width}
+              height={chartStyles.chart.height}
+              theme={VictoryTheme.material}
+              scale={{ x: 'time' }}
+              domainPadding={chartStyles.chart.domainPadding}
+            >
+              <VictoryAxis
+                dependentAxis
+                tickFormat={(tick) => tick * 100 + '%'}
+                style={chartStyles.axis}
+                tickCount={5}
+              />
+              <VictoryAxis
+                style={chartStyles.axis}
+                tickFormat={(tick) => {
+                  return tick.toLocaleString('en-US', {
+                    month: 'short',
+                    day: 'numeric'
+                  });
+                }}
+                tickCount={7}
+              />
+              <VictoryLine
+                data={recentSleepLogs}
+                x={(d) => d.upTime.toDate()}
+                y="sleepEfficiency"
+                style={chartStyles.line}
+                interpolation="monotoneX"
+              />
+              <VictoryScatter
+                data={recentSleepLogs}
+                x={(d) => d.upTime.toDate()}
+                y="sleepEfficiency"
+                style={chartStyles.scatter}
+                size={scale(5)}
+              />
+            </VictoryChart>
+          </View>
         </CardContainer>
       </Container>
     </ScreenContainer>
@@ -35,6 +139,13 @@ export const DiaryStatsScreen = () => {
 
 const styles = StyleSheet.create({
   CardContainer: {
+    marginTop: scale(15),
+    padding: scale(20)
+  },
+  Container_Screen: {
     marginTop: scale(25)
+  },
+  Text_CardTitle: {
+    color: dozy_theme.colors.secondary
   }
 });
