27b9c878a6c16c47b699b1c73a76314e589b54b1
Sam Stowers <phantom4d@gmail.com>
2021-04-16 15:46:37 -0500

    Add streak counter functionality to stats screen

--- screens/DiaryStatsScreen.tsx
+++ screens/DiaryStatsScreen.tsx
@@ -46,17 +46,56 @@ const chartStyles = {
   }
 };
 
+// Return true if date2 is the day before date1 (or same day)
+function isPreviousDay(date1: Date, date2: Date) {
+  const [date1day, date1month, date2day, date2month] = [
+    date1.getDate(),
+    date1.getMonth(),
+    date2.getDate(),
+    date2.getMonth()
+  ];
+  return (
+    (Math.abs(date1day - date2day) <= 1 && // True if same month and day is 1 or 0 away
+      Math.abs(date1month - date2month) === 0) ||
+    (date1month !== date2month && // OR true if different month and date1's day is 1
+      date1day === 1)
+  );
+}
+
 export const DiaryStatsScreen = () => {
   const { state } = React.useContext(AuthContext);
   let allSleepLogs: Array<SleepLog> = state.sleepLogs;
 
-  // Trim sleepLogs to only show most recent 10
+  // Calculate the current diary streak length
+  let streakLength = 0;
+  while (true) {
+    // Check that most recent log is yesterday or today. If not, break here at streak 0
+    if (!isPreviousDay(new Date(), allSleepLogs[0].upTime.toDate())) {
+      break;
+    }
+
+    // Increment streak to start, since zero indexed
+    streakLength++;
+
+    // Check if current log day is the day after previous log day. If so, increment streak by 1. Otherwise, break
+    if (
+      !isPreviousDay(
+        allSleepLogs[streakLength].upTime.toDate(),
+        allSleepLogs[streakLength + 1].upTime.toDate()
+      )
+    ) {
+      break;
+    }
+  }
+
+  // Filter sleepLogs to only show selected month
   let selectedSleepLogs = allSleepLogs.filter(
     (s) =>
       s.upTime.toDate().getMonth() === state.selectedDate.month &&
       s.upTime.toDate().getFullYear() === state.selectedDate.year
   );
 
+  // Helper to orient sleep duration chart properly
   const highestValueForDomain = Math.max.apply(
     Math,
     selectedSleepLogs.map(function (o) {
@@ -82,7 +121,7 @@ export const DiaryStatsScreen = () => {
               ...styles.Text_CardTitle
             }}
           >
-            Sleep log streak: 20
+            Sleep log streak: {streakLength}
           </Text>
         </CardContainer>
         <CardContainer
