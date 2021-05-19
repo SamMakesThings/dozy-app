cbbab570472bc738d6663ca9aca7664a8cc65017
Sam Stowers <phantom4d@gmail.com>
2021-05-19 16:58:12 -0500

    Extend webview screen to accept url or module name

--- screens/TreatmentReviewScreen.tsx
+++ screens/TreatmentReviewScreen.tsx
@@ -6,7 +6,8 @@ import treatments from '../constants/Treatments';
 interface Props {
   route: {
     params: {
-      module: string;
+      module?: string;
+      link?: string;
     };
   };
 }
@@ -15,11 +16,18 @@ const treatmentsIndexed: { [index: string]: { url: string } } = treatments;
 
 // Display a webview of the relevant knowledgebase collection or page
 export const TreatmentReviewScreen: React.FC<Props> = ({ route }) => {
+  let uri = 'https://dozy.customerly.help'; // If no params, show main FAQ
+  if (route.params?.link) {
+    uri = route.params.link;
+  } else if (route.params?.module) {
+    // If module, get the URL from treatment constants
+    uri = treatmentsIndexed[route.params.module].url;
+  }
+
   return (
     <WebView
       source={{
-        // Get the module from nav params & URL from treatment constants
-        uri: treatmentsIndexed[route.params.module].url
+        uri: uri
       }}
     />
   );
