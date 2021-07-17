3848664859af6b05374b9866f813b7db9c62cef6
StartupSam <phantom4d@gmail.com>
2021-07-16 21:03:31 -0700

    Adjust metro.config.js to export entire config

--- metro.config.js
+++ metro.config.js
@@ -1,8 +1,8 @@
 const { getDefaultConfig } = require('@expo/metro-config');
 
-const defaultConfig = getDefaultConfig(__dirname);
+let defaultConfig = getDefaultConfig(__dirname);
 
-module.exports = {
+/* module.exports = {
   transformer: {
     babelTransformerPath: require.resolve('react-native-svg-transformer')
   },
@@ -12,4 +12,16 @@ module.exports = {
     ],
     sourceExts: [...defaultConfig.resolver.sourceExts, 'svg']
   }
-};
+}; */
+
+defaultConfig.resolver.sourceExts.push('svg');
+
+defaultConfig.resolver.assetExts = defaultConfig.resolver.assetExts.filter(
+  (ext) => ext !== 'svg'
+);
+
+defaultConfig.transformer.babelTransformerPath = require.resolve(
+  'react-native-svg-transformer'
+);
+
+module.exports = defaultConfig;
