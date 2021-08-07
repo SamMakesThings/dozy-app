1072eee36d1c41d96218e08929f6399dccaebf57
fancydevpro <41801970+fancydevpro@users.noreply.github.com>
2021-08-07 04:24:36 +0000

    created common utilities

---
+++ utilities/common.ts
@@ -0,0 +1,11 @@
+export function convertAnalyticsEventName(originalName: string): string {
+  const strWithUnderscore = originalName
+    .replace(/[^a-z0-9\s_]/gi, '')
+    .trim()
+    .replace(/\s+_*|_+\s*/g, '_');
+
+  return (/^[0-9]/.test(strWithUnderscore)
+    ? `Event_${strWithUnderscore}`
+    : strWithUnderscore
+  ).substr(0, 40);
+}
