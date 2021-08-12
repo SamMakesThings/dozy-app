8d53bcab7bc63cfa1c60697f86513ed4020bb5f6
fancydevpro <41801970+fancydevpro@users.noreply.github.com>
2021-08-12 10:15:40 +0000

    created crashlytics service module

---
+++ utilities/crashlytics.service.ts
@@ -0,0 +1,32 @@
+import { useEffect } from 'react';
+import crashlytics from '@react-native-firebase/crashlytics';
+
+export class Crashlytics {
+  static useCrashlytics(userId?: string): void {
+    useEffect((): void => {
+      Crashlytics.setUserId(userId);
+    }, [userId]);
+  }
+
+  static async setUserId(id: string): Promise<null> {
+    return crashlytics().setUserId(id);
+  }
+
+  static async setAttribute(name: string, value: string): Promise<null> {
+    return crashlytics().setAttribute(name, value);
+  }
+
+  static async setAttributes(
+    attributes: Record<string, string>
+  ): Promise<null> {
+    return crashlytics().setAttributes(attributes);
+  }
+
+  static log(message: string): void {
+    return crashlytics().log(message);
+  }
+
+  static recordError(error: Error, jsErrorName?: string): void {
+    return crashlytics().recordError(error, jsErrorName);
+  }
+}
