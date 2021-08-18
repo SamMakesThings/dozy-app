864633a5bbba585875cf11cc3159955eba00be8f
fancydevpro <41801970+fancydevpro@users.noreply.github.com>
2021-08-18 00:01:52 +0000

    created separate submit functions

--- utilities/submitOnboardingData.ts
+++ utilities/submitOnboardingData.ts
@@ -3,15 +3,15 @@ import firestore from '@react-native-firebase/firestore';
 import refreshUserData from './refreshUserData';
 import { sub } from 'date-fns';
 
-interface OnboardingState {
+export interface OnboardingState {
   pills: string;
   snoring: boolean;
   rls: boolean;
   parasomnias: boolean;
   otherCondition: boolean;
   expoPushToken: string;
-  diaryReminderTime: Date;
-  firstCheckinTime: Date;
+  diaryReminderTime: Date | null;
+  firstCheckinTime: Date | null;
   diaryHabitTrigger: string;
   ISI1: number;
   ISI2: number;
@@ -47,24 +47,28 @@ export default async function submitOnboardingData(
   // Store the sleep diary notification settings, store generated ID in userData
   // Also set a reminder for the next checkin
   const notifDocRef = userDocRef.collection('notifications').doc();
-  notifDocRef.set({
-    expoPushToken: onboardingState.expoPushToken || 'No push token provided',
-    title: 'How did you sleep?',
-    body: "Add last night's entry now",
-    type: 'DAILY_LOG',
-    time: onboardingState.diaryReminderTime
-      ? onboardingState.diaryReminderTime
-      : new Date(),
-    enabled: onboardingState.diaryReminderTime ? true : false
-  });
-  userDocRef.collection('notifications').add({
-    expoPushToken: onboardingState.expoPushToken || 'No push token provided',
-    title: 'Next checkin is ready',
-    body: 'Open the app now to get started',
-    type: 'CHECKIN_REMINDER',
-    time: onboardingState.firstCheckinTime,
-    enabled: true
-  });
+  if (onboardingState.diaryReminderTime) {
+    notifDocRef.set({
+      expoPushToken: onboardingState.expoPushToken || 'No push token provided',
+      title: 'How did you sleep?',
+      body: "Add last night's entry now",
+      type: 'DAILY_LOG',
+      time: onboardingState.diaryReminderTime
+        ? onboardingState.diaryReminderTime
+        : new Date(),
+      enabled: onboardingState.diaryReminderTime ? true : false
+    });
+  }
+  if (onboardingState.firstCheckinTime) {
+    userDocRef.collection('notifications').add({
+      expoPushToken: onboardingState.expoPushToken || 'No push token provided',
+      title: 'Next checkin is ready',
+      body: 'Open the app now to get started',
+      type: 'CHECKIN_REMINDER',
+      time: onboardingState.firstCheckinTime,
+      enabled: true
+    });
+  }
 
   // Also store reminder info & next check-in datetime
   userDocRef
@@ -114,20 +118,7 @@ export default async function submitOnboardingData(
     });
 
   // Store ISI results
-  const ISIResultsRef = userDocRef.collection('isiResults');
-  ISIResultsRef.add({
-    ISI1: onboardingState.ISI1,
-    ISI2: onboardingState.ISI2,
-    ISI3: onboardingState.ISI3,
-    ISI4: onboardingState.ISI4,
-    ISI5: onboardingState.ISI5,
-    ISI6: onboardingState.ISI6,
-    ISI7: onboardingState.ISI7,
-    ISITotal: onboardingState.ISITotal,
-    timestamp: new Date()
-  }).catch(function (error) {
-    console.error('Error adding ISI data: ', error);
-  });
+  submitISIResults(onboardingState);
 
   // Add initial support chat messages to chat collection
   const chatColRef = userDocRef.collection('supportMessages');
@@ -159,4 +150,111 @@ export default async function submitOnboardingData(
 
   // Manually refresh user data in state once all the above has completed
   refreshUserData(dispatch);
+}
+
+export async function submitISIResults(
+  isiResults: Pick<
+    OnboardingState,
+    'ISI1' | 'ISI2' | 'ISI3' | 'ISI4' | 'ISI5' | 'ISI6' | 'ISI7' | 'ISITotal'
+  >
+): Promise<void> {
+  // Initialize relevant Firebase values
+  let userId = await SecureStore.getItemAsync('userId');
+  let userDocRef =
+    typeof userId === 'string'
+      ? firestore().collection('users').doc(userId)
+      : firestore().collection('users').doc('ERRORDELETEME');
+
+  // Store ISI results
+  const isiResultsToSave = {
+    ISI1: isiResults.ISI1,
+    ISI2: isiResults.ISI2,
+    ISI3: isiResults.ISI3,
+    ISI4: isiResults.ISI4,
+    ISI5: isiResults.ISI5,
+    ISI6: isiResults.ISI6,
+    ISI7: isiResults.ISI7,
+    ISITotal: isiResults.ISITotal,
+    timestamp: new Date()
+  };
+
+  try {
+    const ISIResultsRef = userDocRef.collection('isiResults');
+    const collections = await ISIResultsRef.get();
+    if (collections.docs.length) {
+      await ISIResultsRef.doc(collections.docs[0].id).update(isiResultsToSave);
+    } else {
+      await ISIResultsRef.add(isiResultsToSave);
+    }
+  } catch (error) {
+    console.error('Error adding ISI data: ', error);
+  }
+
+  // Also store reminder info & next check-in datetime
+  try {
+    await userDocRef.update({
+      baselineInfo: {
+        baselineStartDate: new Date(),
+        isiTotal: isiResults.ISITotal
+      }
+    });
+  } catch (error) {
+    console.error('Error adding ISITotal data into user baselineInfo: ', error);
+  }
+}
+
+export async function submitHealthHistoryData(
+  healthHistory: Partial<
+    Pick<
+      OnboardingState,
+      'pills' | 'snoring' | 'rls' | 'parasomnias' | 'otherCondition'
+    >
+  >
+): Promise<void> {
+  // Initialize relevant Firebase values
+  let userId = await SecureStore.getItemAsync('userId');
+  let userDocRef =
+    typeof userId === 'string'
+      ? firestore().collection('users').doc(userId)
+      : firestore().collection('users').doc('ERRORDELETEME');
+
+  try {
+    await userDocRef.update({ healthHistory });
+  } catch (error) {
+    console.error('Error adding health history data: ', error);
+  }
+}
+
+export async function submitDiaryReminderAndCheckinData(
+  diaryAndCheckinData: NonNullable<
+    Pick<
+      OnboardingState,
+      | 'diaryReminderTime'
+      | 'diaryHabitTrigger'
+      | 'firstCheckinTime'
+      | 'expoPushToken'
+    >
+  >
+): Promise<void> {
+  // Initialize relevant Firebase values
+  let userId = await SecureStore.getItemAsync('userId');
+  let userDocRef =
+    typeof userId === 'string'
+      ? firestore().collection('users').doc(userId)
+      : firestore().collection('users').doc('ERRORDELETEME');
+
+  // Store the sleep diary notification settings, store generated ID in userData
+  // Also set a reminder for the next checkin
+  const notifDocRef = userDocRef.collection('notifications').doc();
+  notifDocRef.set({
+    expoPushToken:
+      diaryAndCheckinData.expoPushToken || 'No push token provided',
+    title: 'How did you sleep?',
+    body: "Add last night's entry now",
+    type: 'DAILY_LOG',
+    time: diaryAndCheckinData.diaryReminderTime
+      ? diaryAndCheckinData.diaryReminderTime
+      : new Date(),
+    enabled: diaryAndCheckinData.diaryReminderTime ? true : false
+  });
 }
