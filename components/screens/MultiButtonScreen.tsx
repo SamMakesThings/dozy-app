fa6e634aabdee4092de1e7f6142202373a9493f6
StartupSam <phantom4d@gmail.com>
2021-08-26 17:06:21 -0500

    Fix TS value expectation on multibuttonscreens

--- components/screens/MultiButtonScreen.tsx
+++ components/screens/MultiButtonScreen.tsx
@@ -8,7 +8,7 @@ interface Props {
   questionLabel: string;
   questionSubtitle?: string;
   bottomBackButton: Function;
-  buttonValues: Array<{ label: string; value: string; solidColor: boolean }>;
+  buttonValues: Array<{ label: string; value: string | number | boolean; solidColor: boolean }>;
   onQuestionSubmit: Function;
   theme: Theme;
 }
