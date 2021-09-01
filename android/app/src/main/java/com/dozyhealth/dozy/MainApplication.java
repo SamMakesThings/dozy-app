cb2bcfe4ab691b53c8d5d56b3fd0d37b1ef31286
fancydevpro <41801970+fancydevpro@users.noreply.github.com>
2021-09-01 23:21:59 +0200

    android native configuration for react-native-reanimated

--- android/app/src/main/java/com/dozyhealth/dozy/MainApplication.java
+++ android/app/src/main/java/com/dozyhealth/dozy/MainApplication.java
@@ -10,6 +10,8 @@ import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
+import com.facebook.react.bridge.JSIModulePackage;
+import com.swmansion.reanimated.ReanimatedJSIModulePackage;
 import com.facebook.soloader.SoLoader;
 import com.dozyhealth.dozy.generated.BasePackageList;
 
@@ -49,6 +51,11 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected String getJSMainModuleName() {
       return "index";
+    }
+
+    @Override
+    protected JSIModulePackage getJSIModulePackage() {
+      return new ReanimatedJSIModulePackage();
     }
 
     @Override
