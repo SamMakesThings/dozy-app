f1a1b52a7584faff0f0f349ab47bdeee1692d05a
fancydevpro <41801970+fancydevpro@users.noreply.github.com>
2021-09-17 00:34:50 +0200

    created SettingsScreen test

---
+++ screens/__tests__/SettingsScreen.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import AppManifest from '../../app.json';
+import { findByTestID } from '../../utilities/tests';
+import { SettingsScreen } from '../SettingsScreen';
+
+describe('SettingsScreen', () => {
+  it('should match the snapshot', () => {
+    // Mock new Date() to get consistent date prop for <DatePicker>
+    const mockDate = new Date(Date.UTC(2021, 0, 1, 0));
+    const dateSpy = jest
+      .spyOn(global, 'Date')
+      .mockImplementation(() => mockDate);
+
+    const component = shallow(<SettingsScreen />);
+    expect(component).toMatchSnapshot();
+
+    dateSpy.mockRestore();
+  });
+
+  it('should show the app version correctly', () => {
+    const component = shallow(<SettingsScreen />);
+    expect(findByTestID(component, 'appVersion').props().children).toBe(
+      `@dozyapp ${AppManifest.expo.version}`,
+    );
+  });
+});
