65d9e5e1311538b70d229b8d61400779566b2d47
fancydevpro <41801970+fancydevpro@users.noreply.github.com>
2021-09-24 22:14:19 +0200

    tracked feedback action

---
+++ utilities/feedback.service.ts
@@ -0,0 +1,37 @@
+import React, { useState, useContext, createContext, useMemo } from 'react';
+
+export interface FeedbackContextValue {
+  showingFeedbackPopup: boolean;
+  setShowingFeedbackPopup: (showing: boolean) => void;
+  showingFeedbackWidget: boolean;
+  setShowingFeedbackWidget: (showing: boolean) => void;
+}
+
+class Feedback {
+  static Context = createContext<FeedbackContextValue | null>(null);
+
+  static useFeedbackService(): FeedbackContextValue {
+    const [showingFeedbackPopup, setShowingFeedbackPopup] = useState(true);
+    const [showingFeedbackWidget, setShowingFeedbackWidget] = useState(true);
+
+    const contextValue = useMemo(
+      () => ({
+        showingFeedbackPopup,
+        setShowingFeedbackPopup,
+        showingFeedbackWidget,
+        setShowingFeedbackWidget,
+      }),
+      [showingFeedbackPopup, showingFeedbackWidget],
+    );
+
+    return contextValue;
+  }
+
+  static useFeedback(): FeedbackContextValue {
+    return useContext<FeedbackContextValue>(
+      Feedback.Context as React.Context<FeedbackContextValue>,
+    );
+  }
+}
+
+export default Feedback;
