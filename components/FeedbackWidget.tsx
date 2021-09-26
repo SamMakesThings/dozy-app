3c93473f8bc45a6b0115b7bb3aec5bebbde68ba7
fancydevpro <41801970+fancydevpro@users.noreply.github.com>
2021-09-27 01:49:51 +0200

    updated FeedbackWidget component

--- components/FeedbackWidget.tsx
+++ components/FeedbackWidget.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import {
   View,
   Text,
@@ -6,6 +6,7 @@ import {
   ViewProps,
   TouchableOpacity,
   TextInput,
+  LayoutAnimation,
 } from 'react-native';
 import { withTheme } from '@draftbit/ui';
 import { scale } from 'react-native-size-matters';
@@ -16,6 +17,7 @@ export interface FeedbackWidgetProps extends ViewProps {
   theme: Theme;
   rate: number;
   hasShadow?: boolean;
+  submitted?: boolean;
   onRateChange?: (rate: number) => void;
   onFeedbackChange?: (value: string) => void;
   onSubmit?: () => void;
@@ -26,73 +28,113 @@ export const FeedbackWidget: React.FC<FeedbackWidgetProps> = ({
   style,
   rate,
   hasShadow = false,
+  submitted = false,
   onRateChange,
   onFeedbackChange,
   onSubmit,
   ...props
-}) => (
-  <View
-    style={[
-      hasShadow && theme.elevation[3],
-      styles.container,
-      hasShadow && styles.containerShadow,
-      {
-        borderRadius: theme.borderRadius.global,
-        backgroundColor: theme.colors.medium,
-      },
-      style,
-    ]}
-    {...props}
-  >
-    <Rating
-      showRating
-      fractions={1}
-      ratingTextColor={theme.colors.primary}
-      tintColor={theme.colors.medium}
-      jumpValue={0.5}
-      minValue={0.5}
-      startingValue={0}
-      onFinishRating={onRateChange}
-    />
-    <Text
+}) => {
+  const prevRate = useRef(rate);
+  const [showingInputbox, setShowingInputbox] = useState(false);
+
+  useEffect((): void => {
+    if (prevRate.current === 0 && rate) {
+      LayoutAnimation.configureNext({
+        ...LayoutAnimation.Presets.easeInEaseOut,
+        duration: 300,
+      });
+      setShowingInputbox(true);
+    }
+    prevRate.current = rate;
+  }, [rate]);
+
+  return (
+    <View
       style={[
-        theme.typography.headline6,
-        { color: theme.colors.primary },
-        styles.feedbackTitle,
-      ]}
-    >
-      Please tell us why you gave this rating.
-    </Text>
-    <TextInput
-      style={[
-        styles.feedback,
-        theme.typography.body2,
+        hasShadow && theme.elevation[3],
+        styles.container,
+        hasShadow && styles.containerShadow,
         {
-          backgroundColor: theme.colors.background,
-          color: theme.colors.strong,
+          borderRadius: theme.borderRadius.global,
+          backgroundColor: theme.colors.medium,
         },
+        style,
       ]}
-      multiline
-      numberOfLines={5}
-      onChangeText={onFeedbackChange}
-    />
-    <TouchableOpacity
-      style={[
-        styles.button,
-        { backgroundColor: theme.colors.primary },
-        rate === 0 && styles.disabledButton,
-      ]}
-      disabled={rate === 0}
-      onPress={onSubmit}
+      {...props}
     >
-      <Text
-        style={[theme.typography.button, { color: theme.colors.secondary }]}
-      >
-        Submit
-      </Text>
-    </TouchableOpacity>
-  </View>
-);
+      <Rating
+        showRating
+        fractions={1}
+        ratingTextColor={theme.colors.secondary}
+        tintColor={theme.colors.medium}
+        jumpValue={0.5}
+        minValue={0.5}
+        startingValue={0}
+        readonly={submitted}
+        showReadOnlyText={false}
+        onFinishRating={onRateChange}
+      />
+      {submitted ? (
+        <Text
+          style={[
+            theme.typography.body2,
+            { color: theme.colors.secondary },
+            styles.feedbackTitle,
+          ]}
+        >
+          Thanks for your feedback!
+        </Text>
+      ) : (
+        <>
+          <Text
+            style={[
+              theme.typography.body2,
+              { color: theme.colors.secondary },
+              styles.feedbackTitle,
+            ]}
+          >
+            How likely is it that you would recommend Dozy to a friend?
+          </Text>
+          {showingInputbox && (
+            <>
+              <TextInput
+                style={[
+                  styles.feedback,
+                  theme.typography.body2,
+                  {
+                    backgroundColor: theme.colors.background,
+                    color: theme.colors.strong,
+                  },
+                ]}
+                multiline
+                numberOfLines={5}
+                onChangeText={onFeedbackChange}
+              />
+              <TouchableOpacity
+                style={[
+                  styles.button,
+                  { backgroundColor: theme.colors.primary },
+                  rate === 0 && styles.disabledButton,
+                ]}
+                disabled={rate === 0}
+                onPress={onSubmit}
+              >
+                <Text
+                  style={[
+                    theme.typography.button,
+                    { color: theme.colors.secondary },
+                  ]}
+                >
+                  Submit
+                </Text>
+              </TouchableOpacity>
+            </>
+          )}
+        </>
+      )}
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   // eslint-disable-next-line react-native/no-color-literals
@@ -112,6 +154,7 @@ const styles = StyleSheet.create({
   },
   feedbackTitle: {
     marginTop: scale(16),
+    textAlign: 'center',
   },
   feedback: {
     alignSelf: 'stretch',
@@ -119,7 +162,7 @@ const styles = StyleSheet.create({
     marginTop: scale(8),
     padding: scale(8),
     textAlignVertical: 'top',
-    borderRadius: scale(8),
+    borderRadius: scale(6),
   },
   button: {
     marginTop: scale(24),
