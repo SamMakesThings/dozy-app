3a7226f5b62bd0fa3ddf8e21cb7b3af275cb2e3b
fancydevpro <41801970+fancydevpro@users.noreply.github.com>
2021-10-05 23:29:55 +0200

    added logic in FeedbackWidget

--- components/FeedbackWidget.tsx
+++ components/FeedbackWidget.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from 'react';
+import React, { useRef, useState, useCallback, useEffect } from 'react';
 import {
   View,
   Text,
@@ -7,6 +7,8 @@ import {
   TextInput,
   LayoutAnimation,
   ViewStyle,
+  NativeSyntheticEvent,
+  TextInputSubmitEditingEventData,
 } from 'react-native';
 import { withTheme, Container, Button } from '@draftbit/ui';
 import { scale } from 'react-native-size-matters';
@@ -19,16 +21,17 @@ export interface FeedbackWidgetProps extends ViewProps {
   theme: Theme;
   rate: number;
   submitted?: boolean;
+  defaultValue?: string;
   onRateChange?: (rate: number) => void;
   onFeedbackChange?: (value: string) => void;
-  onSubmit?: () => void;
+  onSubmit: () => void;
 }
 
 export const FeedbackWidget: React.FC<FeedbackWidgetProps> = ({
   theme,
-  style,
   rate,
   submitted = false,
+  defaultValue,
   onRateChange,
   onFeedbackChange,
   onSubmit,
@@ -36,6 +39,15 @@ export const FeedbackWidget: React.FC<FeedbackWidgetProps> = ({
 }) => {
   const prevRate = useRef(rate);
   const [showingInputbox, setShowingInputbox] = useState(rate !== 0);
+
+  const onFeedbackSubmitEditing = useCallback(
+    (event: NativeSyntheticEvent<TextInputSubmitEditingEventData>) => {
+      if (onFeedbackChange) {
+        onFeedbackChange(event.nativeEvent.text);
+      }
+    },
+    [onFeedbackChange],
+  );
 
   useEffect((): void => {
     if (prevRate.current === 0 && rate) {
@@ -48,103 +60,14 @@ export const FeedbackWidget: React.FC<FeedbackWidgetProps> = ({
     prevRate.current = rate;
   }, [rate]);
 
-  /* return (
-    <View
-      style={[
-        hasShadow && theme.elevation[3],
-        styles.container,
-        hasShadow && styles.containerShadow,
-        {
-          borderRadius: theme.borderRadius.global,
-          backgroundColor: theme.colors.medium,
-        },
-        style,
-      ]}
-      {...props}
-    >
-      <Text
-        style={[
-          theme.typography.body2,
-          { color: theme.colors.secondary },
-          styles.feedbackTitle,
-        ]}
-      >
-        How likely is it that you would recommend Dozy to a friend?
-      </Text>
-      <Rating
-        fractions={1}
-        ratingTextColor={theme.colors.secondary}
-        tintColor={theme.colors.medium}
-        jumpValue={0.5}
-        minValue={0.5}
-        startingValue={0}
-        readonly={submitted}
-        showReadOnlyText={false}
-        onFinishRating={onRateChange}
-        style={{flexDirection: 'column-reverse'}}
-      />
-      {submitted ? (
-        <Text
-          style={[
-            theme.typography.body2,
-            { color: theme.colors.secondary },
-            styles.feedbackTitle,
-          ]}
-        >
-          Thanks for your feedback!
-        </Text>
-      ) : (
-        <>
-          {showingInputbox && (
-            <>
-              <TextInput
-                style={[
-                  styles.feedback,
-                  theme.typography.body2,
-                  {
-                    backgroundColor: theme.colors.background,
-                    color: theme.colors.strong,
-                  },
-                ]}
-                multiline
-                numberOfLines={5}
-                onChangeText={onFeedbackChange}
-                placeholder="Optional explanation"
-                placeholderTextColor={theme.colors.lightInverse}
-              />
-              <TouchableOpacity
-                style={[
-                  styles.button,
-                  { backgroundColor: theme.colors.primary },
-                  rate === 0 && styles.disabledButton,
-                ]}
-                disabled={rate === 0}
-                onPress={onSubmit}
-              >
-                <Text
-                  style={[
-                    theme.typography.button,
-                    { color: theme.colors.secondary },
-                  ]}
-                >
-                  Submit
-                </Text>
-              </TouchableOpacity>
-            </>
-          )}
-        </>
-      )}
-    </View>
-  ); */
-
   return (
-    <CardContainer>
+    <CardContainer {...props}>
       <View style={styles.View_CardHeaderContainer}>
         <View style={styles.View_CardHeaderSubContainer}>
           <Text
             style={{
               ...theme.typography.cardTitle,
-              ...styles.Text_CardTitle
+              ...styles.Text_CardTitle,
             }}
           >
             Quick question
@@ -152,10 +75,10 @@ export const FeedbackWidget: React.FC<FeedbackWidgetProps> = ({
           <Text
             style={{
               ...theme.typography.cardTitle,
-              ...styles.Text_RightSubHeader
+              ...styles.Text_RightSubHeader,
             }}
           >
-            6/10
+            {`${rate * 2}/10`}
           </Text>
         </View>
         <Text
@@ -177,45 +100,41 @@ export const FeedbackWidget: React.FC<FeedbackWidgetProps> = ({
           onFinishRating={onRateChange}
           style={styles.Rating}
         />
-        <View style={showingInputbox ? styles.hidden : styles.View_InputBox}>
-          <Container
-            style={{
-              ...styles.View_InputContainer,
-              borderColor: theme.colors.light,
-            }}
-          >
-            <TextInput
-              style={styles.TextInput}
-              placeholder={"Why did you give that rating? (optional)"}
-              placeholderTextColor={theme.colors.light}
-              keyboardType="default"
-              keyboardAppearance="dark"
-              returnKeyType="done"
-              defaultValue={props.defaultValue || undefined}
-              enablesReturnKeyAutomatically={true}
-              onChangeText={onFeedbackChange}
-              onSubmitEditing={(event) => {
-                if (screenState !== States.Valid) {
-                  setTextContent(event.nativeEvent.text);
-                } else {
-                  setTextContent(event.nativeEvent.text);
-                  props.onQuestionSubmit(event.nativeEvent.text);
-                }
+        {submitted ? (
+          <Text style={[theme.typography.body2, styles.Text_CardSubtitle]}>
+            Thanks for your feedback!
+          </Text>
+        ) : (
+          <View style={showingInputbox ? styles.View_InputBox : styles.hidden}>
+            <Container
+              style={{
+                ...styles.View_InputContainer,
+                borderColor: theme.colors.light,
               }}
-            />
-          </Container>
-          <Button
-            style={[
-              theme.buttonLayout as ViewStyle,
-              styles.Button_Submit,
-            ]}
-            type="solid"
-            onPress={onSubmit}
-            color={theme.colors.primary}
-          >
-            Submit feedback
-          </Button>
-        </View>
+            >
+              <TextInput
+                style={styles.TextInput}
+                placeholder={'Why did you give that rating? (optional)'}
+                placeholderTextColor={theme.colors.light}
+                keyboardType="default"
+                keyboardAppearance="dark"
+                returnKeyType="done"
+                defaultValue={defaultValue}
+                enablesReturnKeyAutomatically={true}
+                onChangeText={onFeedbackChange}
+                onSubmitEditing={onFeedbackSubmitEditing}
+              />
+            </Container>
+            <Button
+              style={[theme.buttonLayout as ViewStyle, styles.Button_Submit]}
+              type="solid"
+              onPress={onSubmit}
+              color={theme.colors.primary}
+            >
+              Submit feedback
+            </Button>
+          </View>
+        )}
       </View>
     </CardContainer>
   );
@@ -237,35 +156,35 @@ const styles = StyleSheet.create({
   View_CardHeaderContainer: {
     flexDirection: 'column',
     justifyContent: 'space-between',
-    alignItems: 'flex-start'
+    alignItems: 'flex-start',
   },
   View_CardHeaderSubContainer: {
     flexDirection: 'row',
     width: '100%',
     justifyContent: 'space-between',
-    alignItems: 'center'
+    alignItems: 'center',
   },
   View_CardContentContainer: {
     alignItems: 'flex-start',
     paddingTop: scale(30),
   },
   Text_CardTitle: {
-    color: dozy_theme.colors.secondary
+    color: dozy_theme.colors.secondary,
   },
   Text_RightSubHeader: {
     fontFamily: 'RubikRegular',
     fontSize: scale(17),
     color: dozy_theme.colors.secondary,
-    opacity: 0.5
+    opacity: 0.5,
   },
   Text_CardSubtitle: {
     color: dozy_theme.colors.secondary,
     opacity: 0.5,
     marginTop: scale(0),
-    lineHeight: scale(15)
+    lineHeight: scale(15),
   },
   Rating: {
-    flexDirection: 'column-reverse', 
+    flexDirection: 'column-reverse',
     paddingBottom: scale(23),
   },
   View_InputContainer: {
