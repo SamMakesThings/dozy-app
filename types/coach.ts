5675d5f2625b5312829f52ac116999b15099b07e
fancydevpro <41801970+fancydevpro@users.noreply.github.com>
2021-10-15 18:33:51 +0200

    created coach data types

---
+++ types/coach.ts
@@ -0,0 +1,20 @@
+import { ImageSourcePropType } from 'react-native';
+
+export type CoachImage = ImageSourcePropType;
+
+export interface Coach {
+  id: string; // This id is matched to /coaches/{id}
+  firstName: string;
+  lastName: string;
+  title?: string;
+  image?: CoachImage;
+}
+
+export interface CoachImageDTO {
+  bucket?: string; // Bucket name in the storage
+  path: string; // Image path in the storage
+}
+
+export interface CoachDTO extends Omit<Coach, 'image'> {
+  image?: CoachImageDTO;
+}
