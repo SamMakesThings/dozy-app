2450c5445a751980054ef014a0299ecf236ada97
fancydevpro <41801970+fancydevpro@users.noreply.github.com>
2021-11-19 02:07:48 +0100

    fixed some styles in CardContainer and BaselineProgressCard components

--- components/CardContainer.tsx
+++ components/CardContainer.tsx
@@ -20,7 +20,6 @@ export const CardContainer: React.FC<CardContainerProps> = ({
 const styles = StyleSheet.create({
   View_CardContainer: {
     backgroundColor: dozy_theme.colors.medium,
-    flex: 1,
     padding: scale(12),
     borderRadius: dozy_theme.borderRadius.global,
     marginBottom: scale(15),
