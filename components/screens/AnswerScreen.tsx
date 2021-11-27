cf046a5a1e911b6d4573bfdb122c62b17d8996bf
fancydevpro <41801970+fancydevpro@users.noreply.github.com>
2021-11-27 02:07:12 +0100

    created AnswerScreen

---
+++ components/screens/AnswerScreen.tsx
@@ -0,0 +1,114 @@
+import React from 'react';
+import {
+  StyleProp,
+  StyleSheet,
+  Text,
+  View,
+  ViewStyle,
+  useWindowDimensions,
+} from 'react-native';
+import { withTheme, ScreenContainer, Container } from '@draftbit/ui';
+import { scale } from 'react-native-size-matters';
+import { WebView } from 'react-native-webview';
+import BottomNavButtons from '../BottomNavButtons';
+import { Theme } from '../../types/theme';
+import { FAQData } from '../../types/faq';
+
+export interface AnswerScreenProps {
+  theme: Theme;
+  faq: FAQData;
+  bottomBackButton?: () => void;
+  style?: StyleProp<ViewStyle>;
+}
+
+const AnswerScreen: React.FC<AnswerScreenProps> = ({
+  faq,
+  theme,
+  bottomBackButton,
+  style,
+}) => {
+  const dimensions = useWindowDimensions();
+  console.log('faq: ', faq);
+  return (
+    <ScreenContainer hasSafeArea={true} scrollable={false} style={style}>
+      <Container
+        style={styles.View_ContentContainer}
+        elevation={0}
+        useThemeGutterPadding={true}
+      >
+        {faq.answer?.type === 'plain_text' ? (
+          <>
+            <View style={styles.flexibleContent} />
+            {!!(faq.question.content || faq.answer.content) && (
+              <View style={styles.View_TextContainer}>
+                {!!faq.question.content && (
+                  <Text
+                    style={[
+                      styles.Text_Explainer,
+                      theme.typography.headline5,
+                      {
+                        color: theme.colors.secondary,
+                        marginBottom: scale(10),
+                      },
+                    ]}
+                  >
+                    {faq.question.content}
+                  </Text>
+                )}
+                <Text
+                  style={[
+                    styles.Text_Explainer,
+                    styles.textLabel,
+                    theme.typography.body1,
+                    {
+                      color: theme.colors.secondary,
+                    },
+                  ]}
+                >
+                  {faq.answer.content}
+                </Text>
+              </View>
+            )}
+          </>
+        ) : (
+          <WebView
+            originWhitelist={['*']}
+            source={{ html: faq.answer?.content as string }}
+            style={{ width: dimensions.width * 0.9 }}
+          />
+        )}
+      </Container>
+      <BottomNavButtons
+        bottomBackButton={bottomBackButton}
+        bottomBackButtonLabel="Back"
+        onlyBackButton
+        theme={theme}
+      />
+    </ScreenContainer>
+  );
+};
+
+const styles = StyleSheet.create({
+  View_ContentContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  },
+  View_TextContainer: {
+    width: '90%',
+  },
+  Text_Explainer: {
+    textAlign: 'left',
+    alignItems: 'flex-start',
+  },
+  flexibleContent: {
+    height: scale(20),
+  },
+  textLabel: {
+    marginBottom: 10,
+  },
+});
+
+export default withTheme<AnswerScreenProps, React.FC<AnswerScreenProps>>(
+  AnswerScreen,
+);
