d162aad347ed44ad580ff6d8e94e1a0bf34530ac
fancydevpro <41801970+fancydevpro@users.noreply.github.com>
2022-02-02 03:31:26 +0100

    updated colors in Themes

--- config/Images.js
+++ config/Images.js
@@ -2,5 +2,6 @@ export default {
   UndrawBed: require('../assets/images/UndrawBed.png'),
   DreamBgExtended: require('../assets/images/DreamBgExtended.png'),
   WomanInBed: require('../assets/images/WomanInBed.jpeg'),
-  SamProfile: require('../assets/images/samstowersprofile.jpg')
+  SamProfile: require('../assets/images/samstowersprofile.jpg'),
+  OuraLogo: require('../assets/images/ouraLogo.png'),
 };
