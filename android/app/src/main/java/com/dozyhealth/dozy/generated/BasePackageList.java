6df5070078b19d669a967436a7add00421b8b309
Joshun Cox <joshuncox@gmail.com>
2022-02-25 03:28:39 -0600

    Updated expo to v42. Stable on android

--- android/app/src/main/java/com/dozyhealth/dozy/generated/BasePackageList.java
+++ android/app/src/main/java/com/dozyhealth/dozy/generated/BasePackageList.java
@@ -7,9 +7,8 @@ import org.unimodules.core.interfaces.Package;
 public class BasePackageList {
   public List<Package> getPackageList() {
     return Arrays.<Package>asList(
-        new expo.modules.splashscreen.SplashScreenPackage(),
-        new expo.modules.application.ApplicationPackage(),
         new expo.modules.av.AVPackage(),
+        new expo.modules.application.ApplicationPackage(),
         new expo.modules.constants.ConstantsPackage(),
         new expo.modules.crypto.CryptoPackage(),
         new expo.modules.errorrecovery.ErrorRecoveryPackage(),
@@ -19,8 +18,8 @@ public class BasePackageList {
         new expo.modules.intentlauncher.IntentLauncherPackage(),
         new expo.modules.keepawake.KeepAwakePackage(),
         new expo.modules.notifications.NotificationsPackage(),
-        new expo.modules.permissions.PermissionsPackage(),
         new expo.modules.securestore.SecureStorePackage(),
+        new expo.modules.splashscreen.SplashScreenPackage(),
         new expo.modules.updates.UpdatesPackage(),
         new expo.modules.webbrowser.WebBrowserPackage()
     );
