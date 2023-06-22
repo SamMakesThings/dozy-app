b170e8588b930824b2363abbc05518834ec07157
Sam Stowers <phantom4d@gmail.com>
2023-06-22 14:49:13 -0700

    Re-add snapshot (run with yarn, not npm)

--- src/screens/__tests__/SettingsScreen.test.js
+++ src/screens/__tests__/SettingsScreen.test.js
@@ -5,18 +5,18 @@ import { findByTestID } from '../../utilities/tests';
 import { SettingsScreen } from '../SettingsScreen';
 
 describe('SettingsScreen', () => {
-  // it('should match the snapshot', () => {
-  //   // Mock new Date() to get consistent date prop for <DatePicker>
-  //   const mockDate = new Date(Date.UTC(2021, 0, 1, 0));
-  //   const dateSpy = jest
-  //     .spyOn(global, 'Date')
-  //     .mockImplementation(() => mockDate);
+  it('should match the snapshot', () => {
+    // Mock new Date() to get consistent date prop for <DatePicker>
+    const mockDate = new Date(Date.UTC(2021, 0, 1, 0));
+    const dateSpy = jest
+      .spyOn(global, 'Date')
+      .mockImplementation(() => mockDate);
 
-  //   const component = shallow(<SettingsScreen />);
-  //   expect(component).toMatchSnapshot();
+    const component = shallow(<SettingsScreen />);
+    expect(component).toMatchSnapshot();
 
-  //   dateSpy.mockRestore();
-  // });
+    dateSpy.mockRestore();
+  });
 
   it('should show the app version correctly', () => {
     const component = shallow(<SettingsScreen />);
